73f99cb09f95f66e06eadb1b1519d28c88aad377
Jan Zeugin <jan.zeugin@outlook.com>
2024-04-24 09:57:02 +0200

    Aktualisiere SignUp.vue, um das Anmeldeformular zu ändern und die Registrierungsfunktion hinzuzufügen

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -3,18 +3,18 @@
         <div class="row">
             <div class="col-md-6 offset-md-3">
                 <h1>Sign Up</h1>
-                <form>
+                <form @submit.prevent="signUp">
                     <div class="mb-3">
-                        <label for="DropdownForUserName" class="form-label">Benutzernamen</label>
-                        <input type="text" class="form-control" id="DropdownFormEmail" placeholder="">
+                        <label for="FormEmail" class="form-label">Benutzernamen</label>
+                        <input type="text" class="form-control" id="FormEmail" placeholder="">
                     </div>
                     <div class="mb-3">
-                        <label for="DropdownFormEmail" class="form-label">Email Adresse</label>
-                        <input type="mail" class="form-control" id="DropdownFormEmail" placeholder="">
+                        <label for="FormEmail" class="form-label">Email Adresse</label>
+                        <input type="mail" class="form-control" id="FormEmail" placeholder="">
                     </div>
                     <div class="mb-3">
-                        <label for="DropdownFormPassword" class="form-label">Passwort</label>
-                        <input type="password" class="form-control" id="DropdownFormPassword" placeholder="">
+                        <label for="FormPassword" class="form-label">Passwort</label>
+                        <input type="password" class="form-control" id="FormPassword" placeholder="">
                     </div>
                     <button type="submit" class="btn btn-primary">Sign up</button>
                 </form>
@@ -25,6 +25,27 @@
 
 <script setup>
 
+import ApiService from '@/services/ApiService';
 
+const signUp = () => {
+
+    const username = document.getElementById('FormEmail').value;
+    const email = document.getElementById('FormEmail').value;
+    const password = document.getElementById('FormPassword').value;
+
+    const path = 'http://localhost:3000/auth/register';
+
+    const response = ApiService.post(path, { username, email, password });
+
+    if (response.value === null) {
+        alert('Sign Up fehlgeschlagen');
+        return;
+    }
+
+    console.log(response.value);
+
+    localStorage.setItem("localcashToken", response.value);
+
+}
 
 </script>
